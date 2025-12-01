d469eb27552a77ad8b55f9af91d9ed5e4f376c18
Ekestfa <mustafa.eke@live.com>
2025-12-01 20:26:21 +0300

    dotenv configuration has been added.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-use axum::{
-    routing::get,
-    Router
-};
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use std::env;
 use tokio::net::TcpListener;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::from_filename(".env").ok();
+    let ip = env::var("KODABI_BASE_IP").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("KODABI_BASE_PORT").unwrap_or_else(|_| "8080".to_string());
     // Create a TCP listener on port 3000
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
-    
-    // Create a router with multiple routes
-    let app = Router::new()
-        .route("/health", get(|| async { "OK" }));
+    let listener = TcpListener::bind(format!("{}:{}", ip, port)).await?;
 
-    
+    // Create a router with multiple routes
+    let app = Router::new().route("/health", get(|| async { "OK" }));
+
     // Start serving requests on the listener
-    println!("Server is running on http://0.0.0.0:3000");
-    
+    println!("Server is running on {}:{}", ip, port);
+
     // Serve requests using the router
     axum::serve(listener, app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
         .await?;
-    
+
     Ok(())
 }
 
